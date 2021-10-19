fc1759932852bc6cf2e8ab019b9d5d6ef5572ed7
JohnPaul-Londono <johnplondono@gmail.com>
2021-10-19 17:04:38 -0500

    onclick functions

---
+++ script.js
@@ -0,0 +1,18 @@
+// edit profile name
+function changeName(){
+    document.querySelector("#randomname").innerHTML = 'Kya & Bumi'
+}
+
+
+// these hide the request
+
+function hidetop(id) {
+    var element = document.querySelector("#newfriends1")
+    element.remove()
+}
+
+function hidebtm(id) {
+    var element = document.querySelector("#newfriends2")
+    element.remove()
+}
+
